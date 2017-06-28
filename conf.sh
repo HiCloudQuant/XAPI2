8451c975ae104dac583c4dad6db7c1305eac8415
Kun Ren <renkun@outlook.com>
2017-06-28 16:46:26 +0800

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -1,2 +1,5 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/Workspaces/XAPI2/common/Queue:~/Workspaces/XAPI2/apis/CTP:~/Workspaces/XAPI2/include/CTP/linux64
+#! /bin/bash
+DIR="$(cd "$(dirname "$0")" && pwd)"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DIR/common/Queue:$DIR/apis/CTP:$DIR/include/CTP/linux64
 export MALLOC_CHECK_=0
+
